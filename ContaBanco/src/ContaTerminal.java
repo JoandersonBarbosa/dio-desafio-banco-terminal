5fcfcc2539c410776711109f9653997540db764e
Joanderson <joanderson-oliveira@outlook.com>
2024-08-19 22:00:49 -0300

    meu projeto banco terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -44,4 +44,5 @@ public c//SOLICITANDO ENTRADAS DO USUÁRIO
        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero  + " e seu saldo " + saldo + " já está disponível para saque.");
 
     }
+    
 }
