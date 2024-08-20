6475bc865e09c102d70444364159f614bbc160dc
Joanderson <joanderson-oliveira@outlook.com>
2024-08-19 22:01:55 -0300

    meu projeto conta terminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,26 +1,7 @@
-import java.util.Scanner;
 
 import java.util.Scanner;
 
-public c//SOLICITANDO ENTRADAS DO USUÁRIO
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.print("Digite o seu número da sua conta:");
-        int numero = scanner.nextInt();
-
-        System.out.print("Digite o número da sua agência:");
-        String agencia = scanner.next();
-
-        System.out.print("Digite o seu nome:");
-        String nomeCliente = scanner.next();
-
-        System.out.print("Digite o seu saldo:");
-        Double saldo = scanner.nextDouble();
-
-        scanner.close();
-
-        // IMPRIMINDO TEXTO FINAL COM ENTRADAS DIGITADAS
-       System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero  + " e seu saldo " + saldo + " já está disponível para saque.");
+public class ContaTerminal {
 
     public static void main(String[] args) throws Exception {
         //SOLICITANDO ENTRADAS DO USUÁRIO
@@ -42,7 +23,9 @@ public c//SOLICITANDO ENTRADAS DO USUÁRIO
 
         // IMPRIMINDO TEXTO FINAL COM ENTRADAS DIGITADAS
        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero  + " e seu saldo " + saldo + " já está disponível para saque.");
-
     }
-    
+
 }
+
+
+
