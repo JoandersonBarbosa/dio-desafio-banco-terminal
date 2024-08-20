7674685c5f5371b0b6356574c516627314b35c29
Joanderson <joanderson-oliveira@outlook.com>
2024-08-19 21:52:40 -0300

    meu projeto conta banco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,47 @@
-public class ContaTerminal {
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public c//SOLICITANDO ENTRADAS DO USUÁRIO
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o seu número da sua conta:");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o número da sua agência:");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.print("Digite o seu saldo:");
+        Double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        // IMPRIMINDO TEXTO FINAL COM ENTRADAS DIGITADAS
+       System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero  + " e seu saldo " + saldo + " já está disponível para saque.");
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //SOLICITANDO ENTRADAS DO USUÁRIO
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Digite o seu número da sua conta:");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o número da sua agência:");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o seu nome:");
+        String nomeCliente = scanner.next();
+
+        System.out.print("Digite o seu saldo:");
+        Double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        // IMPRIMINDO TEXTO FINAL COM ENTRADAS DIGITADAS
+       System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero  + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
